763b9b2be94eafe7bf83a28070d98d74f04899fd
devam1135 <devamparekh120@gmail.com>
2025-03-18 18:38:58 +0530

    admin frontend complited

--- adminkit-dev/static/teacher-list-display.php
+++ adminkit-dev/static/teacher-list-display.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="/schoolMonitoring/adminkit-dev/static/css/teacher-list-display.css">
+    <link rel="stylesheet" href="/adminkit-dev/static/css/teacher-list-display.css">
     <title>Teacher Details</title>
     <style>
         /* Pagination */
